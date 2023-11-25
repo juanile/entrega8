c6dc707532be04f00d7289aea5ef2e5b43b8b357
juanile <juanile97@gmail.com>
2023-11-25 14:39:44 -0300

    Pauta 3 solo aplicada a /cart

--- routes/user_cartRoutes.js
+++ routes/user_cartRoutes.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require("jsonwebtoken")
-const SECRET_KEY = "Clave Secreta"
 
-router.use("/", (req, res, next) => {
-    try {
-        const decoded = jwt.verify(req.headers["access-token"], SECRET_KEY);
-        console.log(decoded);
-        next();
-    } catch (err) {
-        res.status(401).json({ message: "Usuario no autorizado"});
-    }
-});
 
 // Endpoint para obtener el archivo JSON de cart
 router.get('/', (req, res) => {
